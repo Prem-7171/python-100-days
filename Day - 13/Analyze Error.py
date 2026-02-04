c3a634b681d791521f13e7f5c55cef67e0c6aa7e
Prem Palkar <157942990+Prem-7171@users.noreply.github.com>
2026-02-04 22:29:07 +0530

    Describing Problem to fix the error

---
+++ Day - 13/Analyze Error.py
@@ -0,0 +1,18 @@
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
+# Describe the Problem - Write your answers as comments:
+# 1. What is the for loop doing?
+# = i take the values from 1 to 19, if i == 20 it prints a statement, but it would never happen as i dosent inherit value 20
+# 2. When is the function meant to print "You got it"?
+#  = when i == 20
+# 3. What are your assumptions about the value of i?
+# = I takes values from 1 to 19, not 20
+
+
+
