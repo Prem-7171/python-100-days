79b975af2f15b4f545fafcb1f1d9674677c01960
Prem Palkar <157942990+Prem-7171@users.noreply.github.com>
2026-02-11 16:21:30 +0530

    Day 15 –  Created a Coffee Machine Project

--- Day - 15/main.py
+++ Day - 15/main.py
@@ -1,12 +1,14 @@
+order = "latte"
 MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
     },
-    "latte": {
+    order: {
         "ingredients": {
             "water": 200,
             "milk": 150,
@@ -25,8 +27,51 @@ MENU = {
 }
 
 resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+    "water": 1000,
+    "milk": 500,
+    "coffee": 300,
+    "money" : 0
 }
+
+
+def report():
+    print(f"Water : {resources["water"]}ml")
+    print(f"Milk : {resources["milk"]}ml")
+    print(f"Coffee : {resources["coffee"]}gm")
+    print(f"Money:{resources["money"]}")
+
+def insert_money():
+    quarters = int(input("How many quarters?:"))
+    dimes = int(input("How many dimes?:"))
+    nickels = int(input("How many nickels?:"))
+    pennies = int(input("How many pennies?:"))
+    return 0.25 * quarters + 0.10 * dimes + 0.05 * nickels + 0.01 * pennies
+
+def machine(orders):
+    if resources["water"] >= MENU[orders]['ingredients']['water'] and resources["milk"] >= MENU[orders]['ingredients']['milk'] and resources["coffee"] >= MENU[orders]['ingredients']['coffee'] :
+        print("Please insert coins.")
+        money_inserted = insert_money()
+        if money_inserted >= MENU[orders]['cost'] :
+            if money_inserted > MENU[orders]['cost']:
+                print(f'Here is ${round(money_inserted - MENU[orders]['cost'], 2)} dollars in change')
+            resources["money"] += 1.50
+            resources["water"] -= MENU[orders]['ingredients']['water']
+            resources["milk"] -= MENU[orders]['ingredients']['milk']
+            resources["coffee"] -= MENU[orders]['ingredients']['coffee']
+            print(f"Here is your {orders} ☕. Enjoy!")
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+    else:
+        print("Sorry there are not enough ingredients.")
+
+while True:
+    order = input("What would you like? (espresso/latte/cappuccino):")
+    if order == "report":
+        report()
+    elif order == "off":
+        break
+    else:
+        machine(order)
+
+
 
